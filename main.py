505b634f7c9b82c8da57e2005885e9037271c1e4
Phan <hoagahphan@gmail.com>
2025-07-23 17:02:21 +0000

    Add Python package structure: setup.py, __init__.py, simplified main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import functions_framework
 from app import create_app
 
 # Create the Flask app
 app = create_app()
 
-@functions_framework.http
-def app_handler(request):
-    """HTTP Cloud Function entry point"""
-    with app.request_context(request.environ):
-        return app.full_dispatch_request()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=False)
